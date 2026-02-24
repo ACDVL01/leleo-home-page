ad6379a885c3352256e36cf032169cee0f739403
ACDVL01 <15995158630@163,com>
2026-02-25 03:27:32 +0800

    更新头像图片

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 const config = {
 	//网页元数据
 	metaData: {
-		title: 'Leleo的个人主页🎉',
-		description: '欢迎来到Leleo的奇妙世界！',
-		keywords: 'Leleo,leleo,个人主页,个人网站',
+		title: '刘濯雨的个人主页🎉',
+		description: '欢迎来到刘濯雨的奇妙世界！',
+		keywords: '刘濯雨,个人主页,个人网站',
 		icon: '/favicon.ico'   //网页图标，支持外链
 	},
 
